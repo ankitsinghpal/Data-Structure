b16e869cb499a2c42728c18356d43184f00ebef0
Ankit Singh Pal <35894301+Mr-Perfectt@users.noreply.github.com>
2019-12-16 04:45:06 -0800

    Create stackimplementation.cpp

---
+++ stackimplementation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int top=-1;
+void push(int arr[], int n, int x)
+{
+    if(top==n-1)
+    {
+        cout<<"\nstack ful"<<top;
+    }
+    else 
+    {
+        top=top+1;
+        arr[top]=x;
+    }
+}
+
+void pop()
+{
+    if(top==-1) cout<<"\nEmpty Stack";
+    else top=top-1;
+}
+int size()
+{
+    return top+1;
+}
+int main() {
+	int n;
+	cin>>n;
+	
+	int arr[n];
+	push(arr,20,n);
+	push(arr,30,n);
+	push(arr,40,n);
+	push(arr,50,n);
+	cout<<size()<<endl;
+	pop();
+	cout<<size();
+	return 0;
+}
