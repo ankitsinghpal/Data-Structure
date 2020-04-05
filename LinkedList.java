7ae5593309d0e96e682fa438774f7129de3d8f2f
Ankit Singh <35894301+ankit-singh-pal@users.noreply.github.com>
2020-04-05 23:51:47 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -239,6 +239,18 @@ public class LinkedList {
         return node;
         
     }
+    static boolean compareList(Node head1, Node head2) {
+        while(head1!=null && head2!=null)
+        {
+            if(head1.data!=head2.data) return false;
+          
+                head1=head1.next;
+   
+                head2=head2.next;
+        }
+        return (head1==null && head2 == null);
+
+    }	
     public static void main(String[] args) {
         LinkedList l=new LinkedList();
         
