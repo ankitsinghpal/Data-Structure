3d0ab2451670ac48711d271455916dac3e4a6850
Ankit Singh <35894301+ankit-singh-pal@users.noreply.github.com>
2020-04-06 00:21:52 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -251,6 +251,21 @@ public class LinkedList {
         return (head1==null && head2 == null);
 
     }	
+    static Node mergeLists(Node head1, Node head2) {
+        if(head1 == null) return head2; 
+        if(head2 == null) return head1; 
+          
+        if(head1.data <= head2.data)  
+        { 
+            head1.next = mergeLists(head1.next, head2); 
+            return head1; 
+        } 
+        else 
+        { 
+            head2.next = mergeLists(head1, head2.next); 
+            return head2; 
+        }    
+    }	
     public static void main(String[] args) {
         LinkedList l=new LinkedList();
         
